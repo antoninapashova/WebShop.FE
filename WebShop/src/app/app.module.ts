9778a7690204da290e521333147cc19e5ebc3041
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-08 17:18:29 +0300

    Setup separate module for angular material modules

--- WebShop/src/app/app.module.ts
+++ WebShop/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { WebShopMaterialModule } from './WebShopMaterialModule';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, WebShopMaterialModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
