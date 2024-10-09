a8d79dbe8c483596602a13a7c10590a0c79d9679
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-09 12:05:42 +0300

    Implement app routing

--- WebShop/src/app/app.module.ts
+++ WebShop/src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { WebShopMaterialModule } from './WebShopMaterialModule';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, WebShopMaterialModule],
-  providers: [provideAnimationsAsync()],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    WebShopMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [provideAnimationsAsync(), provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
