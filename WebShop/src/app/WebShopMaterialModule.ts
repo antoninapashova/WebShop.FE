205a059e53809e38dcc1fb79bfd6ca4099e869e1
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-09 12:03:52 +0300

    Implement signup

--- WebShop/src/app/WebShopMaterialModule.ts
+++ WebShop/src/app/WebShopMaterialModule.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -19,6 +19,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     MatButtonModule,
     MatCardModule,
---
+++ WebShop/src/app/signup/signup.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { matchValidator } from '../password-match-validator';
+import { AuthService } from '../services/auth/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent implements OnInit {
+  signupForm: FormGroup = new FormGroup({});
+  hidePassword = true;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      username: [null, [Validators.required, Validators.minLength(3)]],
+      email: [
+        null,
+        [Validators.required, Validators.minLength(5), Validators.email],
+      ],
+      firstName: [null, [Validators.required, Validators.minLength(3)]],
+      lastName: [null, [Validators.required, Validators.minLength(3)]],
+      password: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          matchValidator('confirmPassword', true),
+        ],
+      ],
+      confirmPassword: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(5),
+          matchValidator('password'),
+        ],
+      ],
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit() {
+    this.authService.register(this.signupForm.value).subscribe({
+      next: () => {
+        this.snackBar.open('Signup successful!', 'Close', { duration: 5000 });
+        this.router.navigateByUrl('/login');
+      },
+      error: () => {
+        this.snackBar.open('Sign up failed. Plese try again', 'Close', {
+          duration: 5000,
+          panelClass: 'eroro-snackbar',
+        });
+      },
+    });
+  }
+}
