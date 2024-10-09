1c0042137405b289033b80e8e122ea6073b853ec
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-10-09 15:59:09 +0300

    Check for user role

--- WebShop/src/app/app.component.ts
+++ WebShop/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { UserStorageService } from './service/storage/user-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'WebShop';
+
+  isCustommerLoggedIn: boolean = UserStorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = UserStorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.isCustommerLoggedIn = UserStorageService.isCustomerLoggedIn();
+      this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+    });
+  }
+
+  logout() {
+    UserStorageService.signOut();
+    this.router.navigateByUrl('login');
+  }
 }
