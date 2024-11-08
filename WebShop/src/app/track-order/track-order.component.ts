351d094827afb4865b55195f0bfb033525d8009f
Antonina Pashova <Antonina.Pashova@arhs-dev-bulgaria.com>
2024-11-08 11:07:48 +0200

    Refactor track order component to show error messages and apply responsive design

--- WebShop/src/app/track-order/track-order.component.ts
+++ WebShop/src/app/track-order/track-order.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-track-order',
@@ -11,7 +12,11 @@ export class TrackOrderComponent {
   searchOrderForm!: FormGroup;
   order: any;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {}
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit() {
     this.searchOrderForm = this.fb.group({
@@ -22,6 +27,14 @@ export class TrackOrderComponent {
   submitForm() {
     this.authService
       .getOrderById(this.searchOrderForm.get('id').value)
-      .subscribe((res) => (this.order = res));
+      .subscribe({
+        next: (res) => (this.order = res),
+        error: (err) => {
+          this.snackBar.open(err.error.message, 'Close', {
+            duration: 5000,
+            panelClass: 'error-snackbar',
+          });
+        },
+      });
   }
 }
